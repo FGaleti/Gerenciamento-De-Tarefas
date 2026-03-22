590b82cf7fc7c1fa2b7e58123435c8108057c2b7
Felipe Galeti Gôngora <fgaletigongora@gmail.com>
2026-03-22 15:03:49 -0300

    Add TarefaModel

---
+++ demo/demo/src/main/java/com/example/demo/model/Tarefa.java
@@ -0,0 +1,72 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity(name = "Tarefa")
+@Table(name = "tb_tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_titulo")
+    private String titulo;
+
+    @Column(name = "ds_tarefa")
+    private String descricao;
+
+    private LocalDate dataVencimento;
+
+    private boolean concluida;
+
+    public Tarefa() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+}
